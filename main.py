74b4576e640964f23bd49ae1d001a96f9a7ff0bc
dazephster <austinlee12378@gmail.com>
2025-07-11 16:05:10 -0500

    Tested with just fractions, weighted avgs, and both. Using both improved on most MAPE scores and improved solution score from 55.56 to 61.7

--- main.py
+++ main.py
@@ -1,39 +1,18 @@
 import pandas as pd
 from src.data_loader import load_data
-from src.model_train import train_model, evaluate_model, run_training, evaluate_model_old
+from src.model_train import train_model, evaluate_model
 from src.predict import save_predictions
-from src.features import compute_blend_weighted_properties
 
 def main():
     
-    # 1.0
-    X_train, y_train, X_test, target_cols = load_data()
+    # 1.2
+    X_train, y_train, X_test, target_cols = load_data(mode='both')
     
     model = train_model(X_train, y_train)
     
-    mape_scores = evaluate_model_old(model, X_train, y_train)
-    
-    print("\nMAPE Scores (lower => better):")
-    for i, score in enumerate(mape_scores):
-        print(f"BlendProperty{i+1}: {score:.4f}")
+    evaluate_model(model, X_train, y_train, target_cols)
         
     save_predictions(model, X_test, target_cols)
-    ###
-    
-    # 1.1
-    # train_df = pd.read_csv('data/train.csv')
-    # model, X_train, y_train, target_cols = run_training(train_df)
-    
-    # test_df = pd.read_csv('data/test.csv')
-    # # X_test = compute_blend_weighted_properties(test_df)
-    
-    # X_test_raw = test_df[[col for col in test_df.columns if 'Component' in col and 'Property' in col]]
-    # X_test_weighted = compute_blend_weighted_properties(test_df)
-    # X_test = pd.concat([X_test_raw, X_test_weighted], axis=1)
-    
-    # save_predictions(model, X_test, target_cols)
-    ###
-    
     return
     
 if __name__ == "__main__":
